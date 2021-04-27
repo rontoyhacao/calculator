cdc00637044cf5d3c2da5092e2359ebe5bcc0b1d
Ron Paolo Toyhacao <rptoyhacao@gmail.com>
2021-04-27 21:14:28 +0800

    Script comment updated

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,21 +1,22 @@
-const input = document.querySelector('input');
+const $input = document.querySelector('input');
 
 // step 1: adding value function on the input
 document.querySelectorAll('.num__key').forEach(el => {
-    el.onclick = () => input.value = input.value !== '0' ? input.value + el.innerText : el.innerText;
+    el.onclick = () => $input.value = $input.value !== '0' ? $input.value + el.innerText : el.innerText;
 });
 
-// step 2: array where operations would be stored
+// step 2: array where operations and values will be stored
 const buffer = [];
 
-// step 4: conversion of input value to float and adding function to the percent operator
+// step 4: parsing of input value to float and adding function to the percent operator
 const operatorCallback = operator => () => {
-    let currentVal = parseFloat(input.value);
+    let currentVal = parseFloat($input.value);
 
     if (operator === 'percent') {
         currentVal *= 0.01;
-        input.value = currentVal;
+        $input.value = currentVal;
     } else {
+        // if something is stored in the buffer 
         if (buffer && buffer.length) {
             buffer.push({ value: currentVal });
 
@@ -24,16 +25,17 @@ const operatorCallback = operator => () => {
             buffer.push({ value: result });
             buffer.push({ value: operator });
 
-            input.value = '';
+            $input.value = '';
         } else {
+            // if the buffer is empty
             buffer.push({ value: currentVal });
             buffer.push({ value: operator });
-            input.value = '';
+            $input.value = '';
         }
     }
 }
 
-// step 5: adding functions to the rest of the operators
+// step 5: retrieval of the inputs from the buffer to be operated
 const evaluate = buffer => {
     const secondOperand = buffer.pop().value;
     const operator = buffer.pop().value;
@@ -45,16 +47,19 @@ const evaluate = buffer => {
             break;
         case 'subtract':
             return firstOperand - secondOperand;
+            break;
         case 'multiply':
             return firstOperand * secondOperand;
+            break;
         case 'divide':
             return firstOperand / secondOperand;
+            break;
         default:
             return secondOperand;
     }
 };
 
-// step 3: calling the operations
+// step 3: calling the operations in an array to a function
 for (const operator of ['add', 'subtract', 'multiply', 'divide', 'percent']) {
     document.querySelector(`.op__key[op=${operator}]`).onclick = operatorCallback(operator);
 }
@@ -62,16 +67,16 @@ for (const operator of ['add', 'subtract', 'multiply', 'divide', 'percent']) {
 // step 6: adding equal function
 document.querySelector('#eq__key').onclick = () => {
     if (buffer && buffer.length) {
-        buffer.push({ value: parseFloat(input.value)});
-        input.value = evaluate(buffer);
+        buffer.push({ value: parseFloat($input.value)});
+        $input.value = evaluate(buffer);
     }
 }
 
 // step 7: adding clear function
 document.querySelector('.op__key[op=clear]').onclick = () => {
-    input.value = 0;
+    $input.value = 0;
     buffer.length = 0;
 };
 
 // step 8: adding negate function
-document.querySelector('.op__key[op=negate]').onclick = () => input.value = -parseFloat(input.value);
+document.querySelector('.op__key[op=negate]').onclick = () => $input.value = -parseFloat($input.value);
